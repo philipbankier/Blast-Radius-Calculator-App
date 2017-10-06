e28abad0d6dc76aa5b7d226e3da7f46e3faba644
philip bankier <philipbankier@gmail.com>
2017-10-06 17:43:03 -0400

    added node-geo for address to coordinate translations

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var NodeGeocoder = require('node-geocoder');
 var app = express();
 var fs      = require('fs');
 var request = require('request');
@@ -50,6 +51,51 @@ app.post('/users', function(req, res) {
     res.send('Check the terminal')
     })
   }
+
+  var options = { 
+    provider: 'google',
+ 
+    // Optional depending on the providers
+    httpAdapter: 'https', // Default
+    // apiKey: 'options.clientIdand options.apiKey', 
+    formatter: null         // 'gpx', 'string', ...
+  };
+ 
+  var geocoder = NodeGeocoder(options);
+   
+  // Using callback
+  geocoder.geocode('29 champs elysée paris', function(err, res) {
+    console.log(res);
+  });
+   
+  // Or using Promise
+  geocoder.geocode('29 champs elysée paris')
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+ 
+// // output :
+// [{
+//   latitude: 48.8698679,
+//   longitude: 2.3072976,
+//   country: 'France',
+//   countryCode: 'FR',
+//   city: 'Paris',
+//   zipcode: '75008',
+//   streetName: 'Champs-Élysées',
+//   streetNumber: '29',
+//   administrativeLevels: {
+//     level1long: 'Île-de-France',
+//     level1short: 'IDF',
+//     level2long: 'Paris',
+//     level2short: '75'
+//   },
+//   provider: 'google'
+// }]
+
 });
 
 
